425476ff934b60f7ecbafd880aef6f22e8c0603d
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-08 22:23:36 +0545

    initialization

---
+++ build.bat
@@ -0,0 +1,12 @@
+@echo off
+cls
+
+if not exist build (
+    mkdir build
+)
+
+cd build
+
+cmake -G "MinGW Makefiles" ..
+
+cmake --build .
---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd build
+
+mingw32-make
+
+output.exe > output.ppm
