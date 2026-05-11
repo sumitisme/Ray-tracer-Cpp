45b045d9360e5df58c21737409c7bad4690fa9fc
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-11 18:10:05 +0545

    created the ray.hpp header

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,15 @@ int main(int argc, char* argv[]) {
     std::cout << "P3\n" << image_width << ' ' <<  image_height << "\n255\n";
 
     for(int j = 0; j < image_height; j++){
+        // For progress report type stuff
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i = 0; i < image_width; i++){
 
-            auto pixel_color = color(double(i)/(image_width - 1), double(j)/(image_height - 1), 0);
+            auto pixel_color = vec3(double(i)/(image_width - 1), double(j)/(image_height - 1), 0);
             write_color(std::cout, pixel_color); // console is the ostream
         }
     }
 
-    std::clog << "\rDone.                   \n";
+    std::clog << "\rDone.\n";
     return 0;
 }
