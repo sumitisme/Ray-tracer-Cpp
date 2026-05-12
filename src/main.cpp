676d420d3e6124d71280af2d2aef5a3d8d65bd87
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-12 14:48:53 +0545

    trying to get the blended scene running

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #include <iostream>
 
 color ray_color(const ray& r) {
-    return color(0, 0, 0);
+    vec3 unit_direction = unit_vector(r.direction()); // Converting the direction into unit
+
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0); // First part is white, second part is skyblue
 }
 
 int main(int argc, char* argv[]) {
@@ -29,7 +32,7 @@ int main(int argc, char* argv[]) {
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
     auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel.
     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
@@ -44,7 +47,7 @@ int main(int argc, char* argv[]) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i = 0; i < image_width; i++){
             auto pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center = camera_center;
+            auto ray_direction = pixel_center - camera_center;
 
             ray r(camera_center, ray_direction);
 
@@ -53,6 +56,6 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    std::clog << "\rDone.\n";
+    std::clog << "\rDone.                   \n";
     return 0;
 }
