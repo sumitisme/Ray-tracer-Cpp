297a51ce887a0412520b21e9a409272b84659199
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-12 07:17:34 +0545

    camera center and viewport

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,37 @@
 
 #include <iostream>
 
+color ray_color(const ray& r) {
+    return color(0, 0, 0);
+}
+
 int main(int argc, char* argv[]) {
     // Image
-    int image_width = 256;
-    int image_height= 256;
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculating image height. Also, making sure it's at least 1
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_width;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
 
     // Render
     std::cout << "P3\n" << image_width << ' ' <<  image_height << "\n255\n";
@@ -16,8 +43,12 @@ int main(int argc, char* argv[]) {
         // For progress report type stuff
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i = 0; i < image_width; i++){
+            auto pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center = camera_center;
 
-            auto pixel_color = vec3(double(i)/(image_width - 1), double(j)/(image_height - 1), 0);
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color); // console is the ostream
         }
     }
