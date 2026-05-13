626ddcdc71515cf9a9c04b6e48e1af8f3d510e70
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-13 17:01:57 +0545

    added sphere

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,20 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin(); // C - P(t)
+    auto a = dot(r.direction(), r.direction()); // d . d
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+
+    if(hit_sphere(point3(0, 0, -1), 0.5, r)) {return color(1, 0, 0);}
+    
     vec3 unit_direction = unit_vector(r.direction()); // Converting the direction into unit
 
     auto a = 0.5 * (unit_direction.y() + 1.0);
