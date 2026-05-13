fa10139eb3f185ff2c7d56b6ead25f280d8ccc09
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-13 16:28:01 +0545

    GRADIENT FIXED OMG OMG OMG OMG

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ color ray_color(const ray& r) {
     vec3 unit_direction = unit_vector(r.direction()); // Converting the direction into unit
 
     auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0); // First part is white, second part is skyblue
+    return ((1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0)); // First part is white, second part is skyblue
 }
 
 int main(int argc, char* argv[]) {
@@ -31,12 +31,12 @@ int main(int argc, char* argv[]) {
     auto viewport_v = vec3(0, -viewport_height, 0);
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
+    vec3 pixel_delta_u = (viewport_u / image_width);
+    vec3 pixel_delta_v = (viewport_v / image_height);
 
     // Calculate the location of the upper left pixel.
     auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
 
     // Render
@@ -46,7 +46,7 @@ int main(int argc, char* argv[]) {
         // For progress report type stuff
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i = 0; i < image_width; i++){
-            auto pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
 
             ray r(camera_center, ray_direction);
