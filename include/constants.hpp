5f1c078e35e0359c0318c4e9e4b26b47edeec93a
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-15 21:05:59 +0545

    common utilities

---
+++ include/constants.hpp
@@ -0,0 +1,31 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+// C++ std usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+// Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.14159265358979323846;
+
+// Utility functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common headers
+
+#include "color.hpp" 
+#include "ray.hpp" 
+#include "vec3.hpp" 
+
+#endif
