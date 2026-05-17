a9e55276e31bb7adc7b8fecfaf93717f1870cfcd
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-17 13:16:43 +0545

    moved the camera into it's own class so the main file is cleaner

--- src/main.cpp
+++ src/main.cpp
@@ -3,100 +3,23 @@
 #include "../include/hittable.hpp"
 #include "../include/hittable_list.hpp"
 #include "../include/sphere.hpp"
+#include "../include/camera.hpp"
 
-//  double hit_sphere(const point3& center, double radius, const ray& r) {
-//      vec3 oc = center - r.origin(); // C - P(t)
-//  //  auto a = dot(r.direction(), r.direction()); // d . d
-//  //  auto b = -2.0 * dot(r.direction(), oc);
-//  //  auto c = dot(oc, oc) - radius * radius;
-//  //  auto discriminant = b * b - 4 * a * c;
-
-//      // Simplification by taking stuff common
-//      auto a = r.direction().length_squared();
-//      auto h = dot(r.direction(), oc); // b = -2h
-//      auto c = oc.length_squared() - radius * radius;
-//      auto discriminant = h * h - a * c;
-
-//      if(discriminant < 0) {
-//          return -1.0;
-//      }
-//      else {
-//  //      return (-b - std::sqrt(discriminant)) / (2.0 * a); 
-//          return (h - std::sqrt(discriminant)) / (a);
-//      }
-//  }
-
-color ray_color(const ray& r, const hittable& world) {
-
-//      auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-//      if(t > 0.0) {
-//          vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-//          return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
-//      }
-
-    hit_record rec;
-    if(world.hit(r, interval(0, infinity), rec)) {
-        return 0.5 * (rec.normal + color(1, 1, 1));
-    }
-    
-    vec3 unit_direction = unit_vector(r.direction()); // Converting the direction into unit
-
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return ((1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0)); // First part is white, second part is skyblue
-}
 
 int main(int argc, char* argv[]) {
-    // Image
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
-
-    // Calculating image height. Also, making sure it's at least 1
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
-    // World
 
     hittable_list world;
 
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100)); // Radius of 100 to make it seem large
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5)); // Location, radius
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
-    auto camera_center = point3(0, 0, 0);
+    camera cam;
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    vec3 pixel_delta_u = (viewport_u / image_width);
-    vec3 pixel_delta_v = (viewport_v / image_height);
-
-    // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
 
 
-    // Render
-    std::cout << "P3\n" << image_width << ' ' <<  image_height << "\n255\n";
+    cam.render(world);
 
-    for(int j = 0; j < image_height; j++){
-        // For progress report type stuff
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for(int i = 0; i < image_width; i++){
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-
-            ray r(camera_center, ray_direction);
-
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color); // console is the ostream
-        }
-    }
-
-    std::clog << "\rDone.                   \n";
     return 0;
 }
