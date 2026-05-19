76907cd1c858b24828221d9d270048c33d13dddd
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-05-19 22:10:49 +0545

    Material updates in its header, sphere, hittable

--- include/sphere.hpp
+++ include/sphere.hpp
@@ -8,9 +8,12 @@ class sphere : public hittable {
     private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 
     public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {
+        // Not initialized yet
+    }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
@@ -41,9 +44,11 @@ class sphere : public hittable {
 
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
+
 };
 
 #endif
